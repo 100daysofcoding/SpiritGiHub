8fa76155285323e14970ab94c8a2295e63115455
Azhar <103309031+SpiritGitHub@users.noreply.github.com>
2024-01-12 22:11:46 +0000

    ajout citation

--- citation/main.py
+++ citation/main.py
@@ -24,6 +24,24 @@ citations = [
     " - Steve Gibson, informaticien américain",
     "La sécurité informatique est une question de comportement, pas de technologie."
     " - Bruce Schneier, informaticien américain"
+    "L'informatique n'est pas seulement une science; c'est une art form,"
+    " dans laquelle l'ingéniosité complète la logique. - Karen Spärck Jones",
+    "Les ordinateurs sont bons pour suivre les instructions, mais pas pour lire dans les pensées. - Donald Knuth",
+    "L'informatique est aussi importante que l'alphabétisation dans notre société moderne. - Marc Andreessen",
+    "Un design bon ne date jamais, un design mauvais est vite oublié. - David Carson",
+    "Le bon design, c'est rendre quelque chose intelligible et mémorable."
+    " Le grand design, c'est rendre quelque chose mémorable et significatif. - Dieter Rams",
+    "L'UX est l'art de raconter une histoire dans un environnement interactif. - Joe Johnston",
+    "La programmation n'est pas seulement apprendre à coder, "
+    "c'est apprendre à penser et à résoudre des problèmes. - Steve Jobs",
+    "Tout le monde devrait apprendre à programmer un ordinateur, parce que cela vous apprend à penser. - Steve Wozniak",
+    "La programmation, c'est comme un art. Chaque ligne de code est comme une pinceau de peinture "
+    "et chaque boucle est comme une nuance de couleur. - Aaron Swartz",
+    "La technologie, comme l'art, est un saut vers de nouvelles hauteurs d'interprétation et de réalisation."
+    " - Edwin Land",
+    "L'innovation technologique nécessite la volonté de faire des erreurs. - Bill Gates",
+    "L'objectif de la technologie n'est pas de confondre l'esprit, mais de servir le corps. - William S. Burroughs"
+    "La technologie est un mot qui décrit quelque chose qui ne fonctionne pas encore. - Douglas Adams",
 ]
 
 
